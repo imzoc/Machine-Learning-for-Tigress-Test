f2cc0b08c4de6e6d7f000b6db5185770cea601d3
Zachary Keyes <zachk@arizona.edu>
2023-01-20 17:08:35 -0700

    Using VSCode for this lol

--- ml4tt/kmodes.py
+++ ml4tt/kmodes.py
@@ -1,6 +1,7 @@
 ''' Time to do k-means! First, import packages and prepare data for processing.
 '''
-from kmodes.kmodes import KModes
+
+from kmodes.kmodes import KModes 
 
 kmodes = KModes(n_clusters=100, init='Huang', n_init=2, verbose=1)
 clusters = kmodes.fit_predict(X)
